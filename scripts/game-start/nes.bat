7cc249cf2a821c7409edbd6b03a0a4e6ba5d361e
b0b33140 <b0b33140@gmail.com>
2021-02-28 17:21:09 +0100

    Add files via upload

---
+++ scripts/game-start/nes.bat
@@ -0,0 +1,10 @@
+@echo off
+
+set system=nes\\
+
+echo.%* | findstr /C:"%system%">nul && (
+"C:\Program Files (x86)\Arduino\hardware\tools\avr/bin/avrdude" "-CC:\Program Files (x86)\Arduino\hardware\tools\avr/etc/avrdude.conf" -v -patmega328p -carduino -PCOM4 -b115200 -D -Uflash:w:C:\Users\Arcade\Documents\Arduino\Programmes_neopixel\nes/nes.ino.standard.hex:i
+    exit
+) || (
+    exit
+)
