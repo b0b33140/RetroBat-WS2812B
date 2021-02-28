afac0e4eae8544f863e05fd34768b4c7e8d00d07
b0b33140 <b0b33140@gmail.com>
2021-02-28 17:25:10 +0100

    Add files via upload

---
+++ Programmes/psx/psx.ino
@@ -0,0 +1,71 @@
+// A basic everyday NeoPixel strip test program.
+
+// NEOPIXEL BEST PRACTICES for most reliable operation:
+// - Add 1000 uF CAPACITOR between NeoPixel strip's + and - connections.
+// - MINIMIZE WIRING LENGTH between microcontroller board and first pixel.
+// - NeoPixel strip's DATA-IN should pass through a 300-500 OHM RESISTOR.
+// - AVOID connecting NeoPixels on a LIVE CIRCUIT. If you must, ALWAYS
+//   connect GROUND (-) first, then +, then data.
+// - When using a 3.3V microcontroller with a 5V-powered NeoPixel strip,
+//   a LOGIC-LEVEL CONVERTER on the data line is STRONGLY RECOMMENDED.
+// (Skipping these may work OK on your workbench but can fail in the field)
+
+#include <Adafruit_NeoPixel.h>
+
+// Which pin on the Arduino is connected to the NeoPixels?
+// On a Trinket or Gemma we suggest changing this to 1:
+#define LED_PIN    3
+
+// How many NeoPixels are attached to the Arduino?
+#define LED_COUNT 50
+
+// Declare our NeoPixel strip object:
+Adafruit_NeoPixel strip(LED_COUNT, LED_PIN, NEO_GRB + NEO_KHZ800);
+// Argument 1 = Number of pixels in NeoPixel strip
+// Argument 2 = Arduino pin number (most are valid)
+// Argument 3 = Pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+
+// setup() function -- runs once at startup --------------------------------
+
+void setup() {
+  strip.begin();           // INITIALIZE NeoPixel strip object (REQUIRED)
+  strip.show();            // Turn OFF all pixels ASAP
+  strip.setBrightness(255); // Set BRIGHTNESS to about 1/5 (max = 255)
+
+}
+
+// loop() function -- runs repeatedly as long as board is on ---------------
+
+void loop() {
+    strip.setPixelColor(0, 66, 73, 73);
+    strip.setPixelColor(1, 66, 73, 73);
+    strip.setPixelColor(2, 66, 73, 73);
+    strip.setPixelColor(3, 66, 73, 73);
+    strip.setPixelColor(4, 0, 114, 67);
+    strip.setPixelColor(5, 255, 0, 255);
+    strip.setPixelColor(6, 0, 56, 123);
+    strip.setPixelColor(7, 193, 18, 28);
+    strip.setPixelColor(8, 66, 73, 73);
+    strip.setPixelColor(9, 66, 73, 73);
+    strip.setPixelColor(10, 0, 0, 0);
+    strip.setPixelColor(11, 0, 0, 0);
+    strip.setPixelColor(12, 66, 73, 73);
+    strip.setPixelColor(13, 66, 73, 73);
+    strip.setPixelColor(14, 0, 114, 67);
+    strip.setPixelColor(15, 255, 0, 255);
+    strip.setPixelColor(16, 0, 56, 123);
+    strip.setPixelColor(17, 193, 18, 28);
+    strip.setPixelColor(18, 66, 73, 73);
+    strip.setPixelColor(19, 66, 73, 73);
+    strip.setPixelColor(20, 0, 0, 0);
+    strip.setPixelColor(21, 0, 0, 0);
+    strip.setPixelColor(22, 66, 73, 73);
+    strip.setPixelColor(23, 66, 73, 73);
+    strip.show();
+   }    
