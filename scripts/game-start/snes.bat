6b4a722aaeb7d04a0f2adc65a687308f713016c5
b0b33140 <b0b33140@gmail.com>
2021-03-08 09:13:53 +0100

    Update snes.bat

--- scripts/game-start/snes.bat
+++ scripts/game-start/snes.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set system=nes\\
+set system=roms\snes\\
 
 echo.%* | findstr /C:"%system%">nul && (
 "C:\Program Files (x86)\Arduino\hardware\tools\avr/bin/avrdude" "-CC:\Program Files (x86)\Arduino\hardware\tools\avr/etc/avrdude.conf" -v -patmega328p -carduino -PCOM4 -b115200 -D -Uflash:w:C:\Users\Arcade\Documents\Arduino\Programmes_neopixel\snes/snes.ino.standard.hex:i
